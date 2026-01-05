c6fd92506b5936f1c01f6cc1e07a33342b3b11b4
Nelis Volschenk <nelisvolschenk@gmail.com>
2026-01-05 08:08:43 +0200

    Initial Commit

--- src/arith.rs
+++ src/arith.rs
@@ -1,57 +1,101 @@
 
 macro_rules! unsigned_impl {
-    ($struct_name: ident, $rhs_type: ty, $inner_type: ty) => {
-        impl core::ops::Add<$rhs_type> for $struct_name {
+    ($struct_name: ident) => {
+        impl core::ops::Add<$struct_name> for $struct_name {
             type Output = Self;
         
-            fn add(self, rhs: $rhs_type) -> Self::Output {
+            fn add(self, rhs: $struct_name) -> Self::Output {
                 Self(self.0.add(rhs.0))
             }
         }
         
-        impl core::ops::Sub<$rhs_type> for $struct_name {
+        impl core::ops::Sub<$struct_name> for $struct_name {
             type Output = Self;
         
-            fn sub(self, rhs: $rhs_type) -> Self::Output {
+            fn sub(self, rhs: $struct_name) -> Self::Output {
                 Self(self.0.sub(rhs.0))
             }
         }
         
-        impl core::ops::Mul<$rhs_type> for $struct_name {
+        impl core::ops::Mul<$struct_name> for $struct_name {
             type Output = Self;
         
-            fn mul(self, rhs: $rhs_type) -> Self::Output {
+            fn mul(self, rhs: $struct_name) -> Self::Output {
                 Self(self.0.mul(rhs.0))
             }
         }
         
-        impl core::ops::Div<$rhs_type> for $struct_name {
+        impl core::ops::Div<$struct_name> for $struct_name {
             type Output = Self;
         
-            fn div(self, rhs: $rhs_type) -> Self::Output {
+            fn div(self, rhs: $struct_name) -> Self::Output {
                 Self(self.0.div(rhs.0))
             }
         }
         
-        impl core::ops::Rem<$rhs_type> for $struct_name {
+        impl core::ops::Rem<$struct_name> for $struct_name {
             type Output = Self;
         
-            fn rem(self, rhs: $rhs_type) -> Self::Output {
+            fn rem(self, rhs: $struct_name) -> Self::Output {
                 Self(self.0.rem(rhs.0))
             }
         }
+
+        impl core::ops::AddAssign<$struct_name> for $struct_name {
+
+            fn add_assign(&mut self, rhs: $struct_name){
+                self.0 = self.0.add(rhs.0)
+            }
+        }
+
+        impl core::ops::SubAssign<$struct_name> for $struct_name {
+
+            fn sub_assign(&mut self, rhs: $struct_name){
+                self.0 = self.0.sub(rhs.0)
+            }
+        }
+
+        impl core::ops::MulAssign<$struct_name> for $struct_name {
+
+            fn mul_assign(&mut self, rhs: $struct_name){
+                self.0 = self.0.mul(rhs.0)
+            }
+        }
+
+        impl core::ops::DivAssign<$struct_name> for $struct_name {
+
+            fn div_assign(&mut self, rhs: $struct_name){
+                self.0 = self.0.div(rhs.0)
+            }
+        }
+
+        impl core::ops::RemAssign<$struct_name> for $struct_name {
+
+            fn rem_assign(&mut self, rhs: $struct_name){
+                self.0 = self.0.rem(rhs.0)
+            }
+        }
+
+
     };
 }
 pub(crate) use unsigned_impl;
 
 macro_rules! signed_impl {
-    ($struct_name: ident, $rhs_type: ty, $inner_type: ty) => {
+    ($struct_name: ident) => {
+        impl core::ops::Neg for $struct_name {
+            type Output = Self;
 
-    }
+            fn neg(self) -> Self::Output {
+                Self(self.0.neg())
+            }
+        }
+    };
 }
+pub(crate) use signed_impl;
 
 macro_rules! unsigned_impl_inner {
-    ($struct_name: ident, $rhs_type: ty, $inner_type: ty) => {
+    ($struct_name: ident, $inner_type: ty) => {
         impl core::ops::Add<$inner_type> for $struct_name {
             type Output = Self;
 
@@ -89,6 +133,41 @@ macro_rules! unsigned_impl_inner {
 
             fn rem(self, rhs: $inner_type) -> Self::Output {
                 Self(self.0.rem(rhs))
+            }
+        }
+
+        impl core::ops::AddAssign<$inner_type> for $struct_name {
+
+            fn add_assign(&mut self, rhs: $inner_type){
+                self.0 = self.0.add(rhs)
+            }
+        }
+
+        impl core::ops::SubAssign<$inner_type> for $struct_name {
+
+            fn sub_assign(&mut self, rhs: $inner_type){
+                self.0 = self.0.sub(rhs)
+            }
+        }
+
+        impl core::ops::MulAssign<$inner_type> for $struct_name {
+
+            fn mul_assign(&mut self, rhs: $inner_type){
+                self.0 = self.0.mul(rhs)
+            }
+        }
+
+        impl core::ops::DivAssign<$inner_type> for $struct_name {
+
+            fn div_assign(&mut self, rhs: $inner_type){
+                self.0 = self.0.div(rhs)
+            }
+        }
+
+        impl core::ops::RemAssign<$inner_type> for $struct_name {
+
+            fn rem_assign(&mut self, rhs: $inner_type){
+                self.0 = self.0.rem(rhs)
             }
         }
     };
--- src/lib.rs
+++ src/lib.rs
@@ -6,16 +6,16 @@ use core::ops::{AddAssign, DivAssign, MulAssign, RemAssign, SubAssign};
 
 #[macro_export]
 macro_rules! new_t {
-    ($name: ident, $inner_type: ty) => {
+    ($name: ident, $inner_type: tt) => {
         new_t!($name,$inner_type,derives = [],impls = []);
     };
-    ($name: ident, $inner_type: ty, derives = [$($extra_derives: tt)*]) => {
+    ($name: ident, $inner_type: tt, derives = [$($extra_derives: tt)*]) => {
         new_t!($name,$inner_type,derives = [$($extra_derives)*],impls = []);
     };
-    ($name: ident, $inner_type: ty, impls = [$($extra_impls: tt)*]) => {
+    ($name: ident, $inner_type: tt, impls = [$($extra_impls: tt)*]) => {
         new_t!($name,$inner_type,derives = [],impls = [$($extra_impls)*]);
     };
-    ($name: ident, $inner_type: ty, derives = [$($extra_derives: tt),*], impls = [$($extra_impls: tt),*]) => {
+    ($name: ident, $inner_type: tt, derives = [$($extra_derives: tt),*], impls = [$($extra_impls: tt),*]) => {
         $(#[derive($extra_derives)])*
         struct $name($inner_type);
         $(impl_block!($extra_impls, $name, $inner_type);)*
@@ -23,11 +23,51 @@ macro_rules! new_t {
 }
 
 macro_rules! impl_block {
-    ("arith", $name: ident, $inner_type:ty) => {
-        crate::arith::unsigned_impl!($name, $name, $inner_type);
+    ("arith", $name: ident, u8) => {
+        crate::arith::unsigned_impl!($name);
     };
-    ("arith_inner", $name: ident, $inner_type:ty) => {
-        crate::arith::unsigned_impl_inner!($name, $inner_type, $inner_type);
+    ("arith", $name: ident, u16) => {
+        crate::arith::unsigned_impl!($name);
+    };
+    ("arith", $name: ident, u32) => {
+        crate::arith::unsigned_impl!($name);
+    };
+    ("arith", $name: ident, u64) => {
+        crate::arith::unsigned_impl!($name);
+    };
+    ("arith", $name: ident, u128) => {
+        crate::arith::unsigned_impl!($name);
+    };
+    ("arith", $name: ident, usize) => {
+        crate::arith::unsigned_impl!($name);
+    };
+    ("arith", $name: ident, i8) => {
+        crate::arith::unsigned_impl!($name);
+        crate::arith::signed_impl!($name);
+    };
+    ("arith", $name: ident, i16) => {
+        crate::arith::unsigned_impl!($name);
+        crate::arith::signed_impl!($name);
+    };
+    ("arith", $name: ident, i32) => {
+        crate::arith::unsigned_impl!($name);
+        crate::arith::signed_impl!($name);
+    };
+    ("arith", $name: ident, i64) => {
+        crate::arith::unsigned_impl!($name);
+        crate::arith::signed_impl!($name);
+    };
+    ("arith", $name: ident, i128) => {
+        crate::arith::unsigned_impl!($name);
+        crate::arith::signed_impl!($name);
+    };
+    ("arith", $name: ident, isize) => {
+        crate::arith::unsigned_impl!($name);
+        crate::arith::signed_impl!($name);
+    };
+    
+    ("arith_inner", $name: ident, $inner_type:tt) => {
+        crate::arith::unsigned_impl_inner!($name, $inner_type);
     }
 }
 
@@ -39,6 +79,14 @@ impl Add for Test {
 
     fn add(self, rhs: Self) -> Self::Output {
         Self(self.0 + rhs.0)
+    }
+}
+
+impl core::ops::Neg for Test {
+    type Output = ();
+
+    fn neg(self) -> Self::Output {
+        todo!()
     }
 }
 
@@ -56,7 +104,7 @@ mod tests {
     use super::*;
     new_t!(Basic, u8);
     new_t!(WithDer, u8, derives = [Clone, Copy]);
-    new_t!(ModbusId, u8, impls = ["arith", "arith_inner"]);
+    new_t!(ModbusId, u32, derives = [Clone, Copy, Debug], impls = ["arith", "arith_inner"]);
 
     #[test]
     fn manual() {
@@ -75,10 +123,21 @@ mod tests {
     }
 
     #[test]
+    fn add_assign() {
+        let mut val1 = ModbusId(5);
+        let val2 = ModbusId(7);
+        val1 += val2;
+        assert_eq!(val1.0, 5+7)
+    }
+
+    #[test]
     fn inner_add() {
         let val1 = ModbusId(5);
         let val2 = 7;
         let v_res = val1 + val2;
         assert_eq!(v_res.0, 5+7)
     }
+
+    
+    
 }
